dae909a51caf5b04b4ad8356ad6bff9141e820bd
Vladskurhak76 <vladskurchak76@gmail.com>
2023-02-15 00:55:36 +0200

    first commit

---
+++ script..js
@@ -0,0 +1,27 @@
+ let myName = prompt("Ведіть своє імя" );
+
+alert(` Вітаємо вас , ${myName} , на нашому сайті`);
+
+const age = 2004;
+console.log(age);
+
+ Skurchak = "Vlad";
+alert(Skurchak);
+
+let Nazar = "Козарчук Назар Меланхоліх"
+console.log(Nazar);
+
+Nazar1 = "Козарчук Назар Меланхоліх"
+console.log(Nazar1);
+
+let city = ["Калуш", "Івано-Франківськ", "Коломия"];
+console.log(city);
+
+country= ["Україна", "Польща", "Молдова"];
+console.log(country);
+
+console.log(myName);
+
+result = confirm("Повнолітній?");
+
+result = prompt("ti");
---
+++ script2.js
@@ -0,0 +1,4 @@
+elements2=document.getElementsByTagName('p');
+for(var i=0;i<elements2.length;i++){
+elements2[i].style.color="red";
+}
